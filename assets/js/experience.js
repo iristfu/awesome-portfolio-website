9e8e6ad79d7b9bd91ce752940b2bf5bb61565f21
iristfu <62958926+iristfu@users.noreply.github.com>
2021-08-12 02:57:58 -0700

    added research intern experience

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -6,11 +6,11 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
+    title: "Research Intern",
     cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    place: "Stanford Human Computer Interaction Lab",
+    time: "(April, 2021 - present)",
+    desp: "<p>I am an undergraduate research intern through the CURIS program this summer. Specifically, I'm building out a narrative-based augmented reality tutor, which integrates AI-generated narratives with AR learning activities. I'm using Unity and C# to create interactive features such as a 3D planet model and a 3D diary that stores text and image input from users. </p>",
   },
   {
     title: "Student Developer",
