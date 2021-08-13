16c4622c500acf41fc82d50013f6bd8b95c0c98f
iristfu <62958926+iristfu@users.noreply.github.com>
2021-08-13 00:13:54 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,7 +19,7 @@ let header = $(`
   <ul class="navbar-nav ml-auto" id = "navbar-content">
    <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Home</a></li>
    <li class="nav-item nav-item-hover" onclick="scrollToExperienceSection()"><a class="nav-link" href="#work-experience">Experience</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="#my-projects">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="https://hashnode.com/" target="_blank">Blogs</a></li>
