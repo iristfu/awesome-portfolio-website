a0efc05ebb7c0e8c696d2014929a36b3d439cf18
iristfu <62958926+iristfu@users.noreply.github.com>
2021-08-18 21:29:58 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -145,8 +145,8 @@ let footer = $(`
     
 
     <div class="rounded-social-buttons tag">
-    
-    <a class="social-button linkedin" href="" target="_blank" >
+
+    <a class="social-button linkedin" href="https://bit.ly/2VBtbAl" target="_blank" >
     <!-- svg code for linkedin icon -->
       <svg class="linkedin-icon-footer" xmlns="http://www.w3.org/2000/svg" width="35" viewBox="0 0 24 24" fill="#0e76a8"
         stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
@@ -155,8 +155,6 @@ let footer = $(`
         <circle cx="4" cy="4" r="2"></circle>
       </svg>
     </a>
-
-
 
     <a class="social-button instagram" href="" target="_blank" >
     <!-- svg code for instagram icon -->
