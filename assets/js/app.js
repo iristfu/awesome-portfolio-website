1e01ea34322f90d23cf0840902ec404ac4bc72a8
iristfu <62958926+iristfu@users.noreply.github.com>
2021-08-18 21:31:21 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -146,7 +146,7 @@ let footer = $(`
 
     <div class="rounded-social-buttons tag">
 
-    <a class="social-button linkedin" href="https://bit.ly/2VBtbAl" target="_blank" >
+    <a class="social-button linkedin" href="https://bit.ly/3s4D9Ww" target="_blank" >
     <!-- svg code for linkedin icon -->
       <svg class="linkedin-icon-footer" xmlns="http://www.w3.org/2000/svg" width="35" viewBox="0 0 24 24" fill="#0e76a8"
         stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
@@ -156,7 +156,7 @@ let footer = $(`
       </svg>
     </a>
 
-    <a class="social-button instagram" href="" target="_blank" >
+    <a class="social-button instagram" href="https://bit.ly/instagram-irisfu" target="_blank" >
     <!-- svg code for instagram icon -->
       <svg class="instagram-icon-footer" x="0px" y="0px" viewBox="0 0 202.5 202.5"
         style="enable-background:new 0 0 202.5 202.5;">
@@ -167,7 +167,7 @@ let footer = $(`
       </svg>
     </a>
 
-    <a class="social-button github" href="#" target="_blank">
+    <a class="social-button github" href="https://bit.ly/2VBtbAl" target="_blank">
     <!-- SVG code for Github icon -->
     <svg class="github-icon-footer" width="45px" height="45px" viewBox="0 0 300 300">
         <!-- body -->
